f7e4d54ac4293ca77aca2353af489605a29fcda1
R. Mark Sharp <msharp@TxBiomed.org>
2017-08-23 11:53:10 -0500

    Added ability to place the report into a directory of choice.

--- R/rename_report.R
+++ R/rename_report.R
@@ -6,13 +6,16 @@
 #'
 #' @return Character vector of length one having the new date and time
 #' stamped file name.
+#' @param path character vector of length one containing the path to add to the
+#' new file name.
 #' @import rmsutilityr
+#' @import stringi
 #' @export
-rename_report <- function() {
+rename_report <- function(path = "") {
   old_name <- "renameSurgerySheets.pdf"
   new_name <- ""
   if (file.exists(old_name)) {
-    new_name <- get_dated_filename("Surgery_Sheet_Report.pdf")
+    new_name <- stri_c(path, get_dated_filename("Surgery_Sheet_Report.pdf"))
     status <- file.rename(old_name, new_name)
     if (!status)
       cat(stri_c("\nFailed to rename ", old_name, " to ", new_name, ".\n"))
